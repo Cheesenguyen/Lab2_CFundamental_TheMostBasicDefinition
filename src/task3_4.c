1ed0751b2eb777f905a060dc5bd93be7ea912f56
Cheesenguyen <nguyenthuychi0206@gmail.com>
2025-01-26 15:17:30 +0000

    update task 3-4

--- src/task3_4.c
+++ src/task3_4.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>  
+
 int main() {
     char str[215];
-    scanf("%[a-zA-Z ]", str);
+    char ch;
+    int i = 0;
+
+    while (scanf("%c", &ch) && !isdigit(ch)) {
+        str[i++] = ch;  // Lưu ký tự vào chuỗi
+    }
+
+    str[i] = '\0';  
+
     printf("%s\n", str);
+
     return 0;
 }
