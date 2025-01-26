d8196cf951b6a662b2ad49d5f628ee42df70238a
Cheesenguyen <nguyenthuychi0206@gmail.com>
2025-01-26 15:01:16 +0000

    update task 3-6

--- src/task3_6.c
+++ src/task3_6.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 int main() {
-    char prefix[11], name[91];
-    scanf("%10s %[^\n]", prefix, name);
+    char name[101]; 
+    scanf("Full Name: %[^\n]", name);
     printf("%s\n", name);
+
     return 0;
 }
