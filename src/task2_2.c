214b407407af1c47f0cf30906cea712a5fc09b90
Cheesenguyen <nguyenthuychi0206@gmail.com>
2025-01-28 03:19:33 +0000

    update task 2_2

--- src/task2_2.c
+++ src/task2_2.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
+// Hàm in ký tự lặp lại
+void print_line(char ch, int length) {
+    for (int i = 0; i < length; i++) {
+        putchar(ch);
+    }
+    putchar('\n');
+}
+
 int main() {
-    // Define file attributes
-    unsigned int fileAddress = 4152963;
-    char filePath[] = "C:\\User\\Administrator\\Downloads\\file.txt";
-    char fileContent[] = "Hello World!";
-    char author[] = "Tran Quang Huy";
-    char fileSize[] = "4KB";
-    char dateCreated[] = "19/03/2024";
-    char dateModified[] = "20/03/2024";
+    // In phần tiêu đề với đường kẻ
+    print_line('=', 40);
+    printf("%25s\n", "FILE REPORT");
+    print_line('=', 40);
 
-    // Constants for formatting
-    const char *line = "========================================";
+    // In các thông tin còn lại
+    printf("%-20s%s\n", "File address:", "3F5E83");
+    printf("%-20s%s\n", "Path:", "C:\\User\\Administrator\\Downloads\\file.txt");
+    printf("%-20s\"%s\"\n", "Content:", "Hello World!");
+    printf("%-20s%s\n", "Author:", "Tran Quang Huy");
+    printf("%-20s%s\n", "Size:", "4KB");
 
-    // Print the formatted report
-    printf("%s\n", line);
-    printf("%26sFILE REPORT\n", ""); // Center align "FILE REPORT"
-    printf("%s\n", line);
-    printf("%-20s%X\n", "File address:", fileAddress); // Hex format for address
-    printf("%-20s%s\n", "Path:", filePath);
-    printf("%-20s\"%s\"\n", "Content:", fileContent); // Quoted content
-    printf("%-20s%s\n", "Author:", author);
-    printf("%-20s%s\n\n", "Size:", fileSize); // Extra newline for metadata
-
-    printf("Metadata:\n");
-    printf("%-20s%s\n", "Date created:", dateCreated);
-    printf("%-20s%s\n", "Date modified:", dateModified);
+    // Metadata
+    printf("\nMetadata:\n");
+    printf("%-20s%s\n", "Date created:", "19/03/2024");
+    printf("%-20s%s\n", "Date modified:", "20/03/2024");
 
     return 0;
 }
